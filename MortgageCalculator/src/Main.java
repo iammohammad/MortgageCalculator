899ecc932288054a4ece9e768f59532885ac7a19
mohammad <moohammada73.ah@gmail.com>
2024-07-17 17:37:02 +0330

    write the first step of mortgage calculator

--- MortgageCalculator/src/Main.java
+++ MortgageCalculator/src/Main.java
@@ -1,5 +1,30 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        final byte MONTH_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.println("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
+
+        System.out.println("Period (YEARS): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTH_IN_YEAR;
+
+        double mortgage = principal
+                *(monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
